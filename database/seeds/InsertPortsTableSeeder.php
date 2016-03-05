87ac364ecfe9217784866da44193109597c3d9c8
laf <neil@lathwood.co.uk>
2016-03-05 10:38:09 +0000

    Added device_id to port create

--- database/seeds/InsertPortsTableSeeder.php
+++ database/seeds/InsertPortsTableSeeder.php
@@ -15,7 +15,8 @@ class InsertPortsTableSeeder extends Seeder
     {
         $devices = Device::all();
         foreach ($devices as $device) {
-            $data  = ['ifIndex' => 1, 'ifDescr' => 'eth0', 'ifName' => 'eth0', 'ifSpeed' => 1000000, 'ifOperStatus' => 'up', 'ifAdminStatus' => 'up', 'ifType' => 'ethernetCsmacd', 'ifAlias' => 'eth0'];
+            $device_id = $device->device_id;
+            $data  = ['device_id' => $device_id, 'ifIndex' => 1, 'ifDescr' => 'eth0', 'ifName' => 'eth0', 'ifSpeed' => 1000000, 'ifOperStatus' => 'up', 'ifAdminStatus' => 'up', 'ifType' => 'ethernetCsmacd', 'ifAlias' => 'eth0'];
             $device->ports()->create($data);
         }
     }
