aa90c9fadd6dc71bbcf9736618a7308ba2faafa8
Tony Murray <murraytony@gmail.com>
2016-03-07 00:49:21 -0600

    Initial PhpStorm IDE files

---
+++ .idea/fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,8 @@
+/**
+ * ${FILE_NAME}
+ *
+ * @package    LibreNMS
+ * @author     Author Name <email>
+ * @copyright  ${YEAR} Author Name
+ * @license    @license http://opensource.org/licenses/GPL-3.0 GNU Public License v3 or later
+ */
