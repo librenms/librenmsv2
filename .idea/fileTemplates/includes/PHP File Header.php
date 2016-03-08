dd9a6d10d5998b83d31e6edca29bcfa43300c573
Tony Murray <murraytony@gmail.com>
2016-03-07 19:12:03 -0600

    Remove .idea from the repo and modify .gitignore

--- .idea/fileTemplates/includes/PHP File Header.php
+++
@@ -1,8 +0,0 @@
-/**
- * ${FILE_NAME}
- *
- * @package    LibreNMS
- * @author     Author Name <email>
- * @copyright  ${YEAR} Author Name
- * @license    @license http://opensource.org/licenses/GPL-3.0 GNU Public License v3 or later
- */
